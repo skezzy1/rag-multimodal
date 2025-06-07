9864c39f9c4775568f314750d4061bf33ae197a5
Serhii Herasymchuk <serhiy1204@gmail.com>
2025-06-07 17:32:47 +0300

    Security updation

--- module/gemini_module.py
+++ module/gemini_module.py
@@ -3,7 +3,7 @@ import streamlit as st
 import google.generativeai as genai
 from dotenv import load_dotenv
 load_dotenv()
-genai.configure(api_key = os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key = os.getenv("GEMINI_API_KEY"))
 
 model = genai.GenerativeModel("models/gemini-1.5-flash")
 
